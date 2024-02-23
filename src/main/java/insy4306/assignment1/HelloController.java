2b153282f1cc8bba0a785d876cb9f961076eecc3
Austin Ramey <austinramey98@gmail.com>
2024-02-22 18:09:24 -0600

    Revision 2-22 in class

--- src/main/java/insy4306/assignment1/HelloController.java
+++ src/main/java/insy4306/assignment1/HelloController.java
@@ -2,16 +2,22 @@ package insy4306.assignment1;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private RadioButton gameButton;
+    @FXML
+    private RadioButton prodButton;
+    @FXML
+    private RadioButton eduButton;
+
     public void initialize(){
-        
+        gameButton.setSelected(true);
     }
+
+
+
 }
