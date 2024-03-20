7c8fc87d2e8e05d5755dde21db83775070085495
Austin Ramey <austinramey98@gmail.com>
2024-03-20 18:05:21 -0500

    Working gray out function on the buttons

--- src/main/java/insy4306/assignment1/HelloController.java
+++ src/main/java/insy4306/assignment1/HelloController.java
@@ -1,8 +1,7 @@
 package insy4306.assignment1;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.*;
 
 public class HelloController {
     @FXML
@@ -13,11 +12,32 @@ public class HelloController {
     private RadioButton prodButton;
     @FXML
     private RadioButton eduButton;
+    @FXML
+    private CheckBox appButton;
+    @FXML
+    private CheckBox musicButton;
+    @FXML
+    private Label labelTypeApp;
+    // @FXML
+    // private ComboBox comboBox;
 
+    @FXML
     public void initialize(){
         gameButton.setSelected(true);
     }
 
-
-
+    @FXML
+    public void handleChooseOne() {
+        if (appButton.isSelected()) {
+            labelTypeApp.setStyle("-fx-text-fill: gray");
+            gameButton.setDisable(true);
+            prodButton.setDisable(true);
+            eduButton.setDisable(true);
+        } else {
+            labelTypeApp.setStyle("-fx-text-fill: black");
+            gameButton.setDisable(false);
+            prodButton.setDisable(false);
+            eduButton.setDisable(false);
+        }
+    }
 }
