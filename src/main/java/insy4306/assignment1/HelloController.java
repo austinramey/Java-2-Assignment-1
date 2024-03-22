1309c6b023bdb42f430fc8bda8a432b5b2c977be
Austin Ramey <austinramey98@gmail.com>
2024-03-21 23:06:40 -0500

    Added moving data to txt file

--- src/main/java/insy4306/assignment1/HelloController.java
+++ src/main/java/insy4306/assignment1/HelloController.java
@@ -1,7 +1,10 @@
 package insy4306.assignment1;
 
+import java.io.FileWriter;
+import java.nio.file.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 
 public class HelloController {
     @FXML
@@ -22,6 +25,24 @@ public class HelloController {
     private Label labelTypeMusic;
     @FXML
     private ComboBox<String> comboMusicBox;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField streetField;
+    @FXML
+    private TextField cityField;
+    @FXML
+    private TextField stateField;
+    @FXML
+    private TextField zipcodeField;
+    @FXML
+    private TextField titleField;
+    @FXML
+    private TextField dateField;
+    @FXML
+    private TextField accountField;
+    @FXML
+    private boolean errorCheck=true;
 
     @FXML
     public void initialize(){
@@ -58,5 +79,96 @@ public class HelloController {
         gameButton.setDisable(false);
         prodButton.setDisable(false);
         eduButton.setDisable(false);
+    }
+
+    @FXML
+    public void handleSubmit(){
+        try {
+            if(appButton.isSelected()){
+                // Path path = Paths.get("app.txt");
+                // String appOutput = appOutput();
+                // Files.write(path, appOutput.getBytes(), StandardOpenOption.APPEND);
+                FileWriter writer = new FileWriter("app.txt");
+                writer.write(appOutput());
+                writer.close();
+            } else if (musicButton.isSelected()) {
+                // Path path = Paths.get("music.txt");
+                // String musicOutput = musicOutput();
+                // Files.write(path, musicOutput.getBytes(), StandardOpenOption.APPEND);
+                FileWriter writer = new FileWriter("music.txt");
+                writer.write(musicOutput());
+                writer.close();
+            } else {
+                nameField.requestFocus();
+                Alert alert = new Alert(AlertType.ERROR, "Neither App nor Music Selected!");
+                alert.showAndWait();
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    @FXML
+    public String appOutput() {
+        String returnString = "";
+        while(errorCheck) {
+            returnString += checkEmpty(nameField) + " | ";
+            returnString += checkEmpty(streetField) + " | ";
+            returnString += checkEmpty(cityField) + " | ";
+            returnString += checkEmpty(stateField) + " | ";
+            returnString += checkEmpty(zipcodeField) + " | ";
+            returnString += checkEmpty(titleField) + " | ";
+            returnString += checkEmpty(dateField) + " | ";
+            returnString += checkEmpty(accountField) + " | ";
+            returnString += "\n";
+            break;
+        }
+        if(errorCheck) {
+            return returnString;    
+        } else {
+            errorCheck = true;
+            return "";
+        }
+    }
+
+    @FXML
+    public String musicOutput() {
+        String returnString = "";
+        while(errorCheck) {
+            returnString += checkEmpty(nameField) + " | ";
+            returnString += checkEmpty(streetField) + " | ";
+            returnString += checkEmpty(cityField) + " | ";
+            returnString += checkEmpty(stateField) + " | ";
+            returnString += checkEmpty(zipcodeField) + " | ";
+            returnString += checkEmpty(titleField) + " | ";
+            returnString += checkEmpty(dateField) + " | ";
+            returnString += checkEmpty(accountField) + " | ";
+            returnString += "\n";
+            break;
+        }
+        if(errorCheck) {
+            return returnString;    
+        } else {
+            errorCheck = true;
+            return "";
+        }
+    }
+
+    @FXML
+    public String checkEmpty(TextField textField) {
+        if(textField.getText().trim().isEmpty()) {
+            Alert alert = new Alert(AlertType.ERROR, "One of your fields is empty");
+            alert.showAndWait();
+            textField.requestFocus();
+            return "";
+        } else {
+            errorCheck=true;
+            return textField.getText();
+        }
+    }
+    
+    @FXML
+    public void handleFinish() {
+        System.exit(0);
     }
 }
